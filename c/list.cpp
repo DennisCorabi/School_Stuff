fa1c9b379e108b7f0e5494b32dce2f369c4eafeb
Dennis Corabi <denniscorabi@outlook.com>
2021-10-10 19:05:24 +0200

    aggiunte due funzioni + commenti

--- c/list.cpp
+++ c/list.cpp
@@ -9,22 +9,53 @@ struct node{
 };
 
 void printList(node * testa);
-node *  addlist(int element, node * head);
-void addAt(int element, int index, node * head);
+node *  addlist(node * head);
+void addAt(node * head);
 node * elementAt(int indice, node * head);
+int sizeList(node * head);
+void sortList(node * head);
 
 /*
 FUNZIONE main:
-    funzione principale utilizzata in questo programma sopratutto come luogo per i test."
+    funzione principale utilizzata in questo programma come menu per accedere alle varie funzioni.
     */
 
 int main(void){
     node *lista = NULL;
-    lista=addlist(1,lista);
-    lista=addlist(10,lista);
-    lista=addlist(5,lista);
-    addAt(7,2,lista);
-    printList(lista);
+    int scelta;
+    do{
+        printf("\ncosa vuoi fare?\n");
+        printf("1: aggiungi nodi alla lista.\n2:stampa contentuto di una lista.\n");
+        printf("3: sostituisci un elemento in un nodo della lista\n4:ordina gli elementi della lista.\n");
+        printf("5: crea ed inserisce tanti nodi quanti ne vuoi!\n0: esci dal programma.\n");
+        printf("scelta: ");
+        scanf("%d",&scelta);
+        switch(scelta){
+            case 1:
+                lista=addlist(lista);
+                printf("\n\nnodo aggiunto nella lista.\n\n");
+                break;
+            case 2:
+                printList(lista);
+                break;
+            case 3:
+                addAt(lista);
+                printf("nodo sostituito.\n\n");
+                break;
+            case 4:
+                sortList(lista);
+                break;
+            case 5:
+                printf("ciao");
+            case 0:
+                break;
+            default:
+                printf("inserisci un numero corrispondente alle varie opzioni!\n");
+                break;
+                
+        }
+
+    }while(scelta!=0);
 }
 
 /*
@@ -32,16 +63,17 @@ FUNZIONE addlist:
     creazione di un nuovo nodo ed inserimento dello stesso nella lista la cui "testa"
     (ultimoè passata alla funzione come secondo argomento.*/
 
-node* addlist(int element, node *head){
+node* addlist(node *head){
     node * newnode;                 //creazione nuovo nodo (da poi inserire nella lista).
     node *temp =head;               //creazione nodo temporaneo (utilizzato nel ciclo while per non toccare 'head'.)
     newnode = (node*) malloc(sizeof(struct node));  //alloco dello spazio nella ram per il nuovo nodo
-    newnode->elemento =element; //inserisco come contenuto nel nodo il numero passato alla funzione come primo argomento.
-    newnode->next = NULL;   //il nuovo nodo punterà ovviamente al vuoto (per ora).
+    printf("inserisci un numero: ");  //inserisco come contenuto nel nodo un numero scelto dall'utente.
+    scanf("%d",&newnode->elemento);
+    newnode->next = NULL;   //il nuovo nodo punterà al vuoto (per ora).
 
     if (head == NULL) {             //se stiamo inserendo il primo elemento della lista, allora lo inserisco direttamente all'inizio della lista senza dover ricercare la "testa" di essa.
         newnode->index=0;           //sarà il primo nodo della lista, quindi 0.
-        head = newnode;             //copio il contenuto del nuovo nodo (numero, indice e puntatore) nella testa della lista.
+        head = newnode;             //copio il contenuto del nuovo nodo (numero, indice e puntatore) nella base della lista.
         return head;
     }
     int i=1;            //creo variabile che mi conterà quanti nodi ha la lista ad ogni inserimento
@@ -53,15 +85,25 @@ node* addlist(int element, node *head){
     temp->next = newnode;           //dopo aver trovato la fine della lista, aggiungo un altro nodo contenente le informazioni di newnode.
     return head;
 }
-
 /*
 FUNZIONE addAt:
     sostituzione dell'elemento di un nodo dato il suo indice e la lista a cui appartiene.
 */
 
-void addAt(int element, int indice, node * head){
-    node * temp=elementAt(indice,head);
-    temp->elemento=element;     //sostituisco il valore del nodo con l'indice ricercato.
+void addAt(node * head){
+    int index,size=sizeList(head),sostituto;
+
+    do{
+        printf("inserisci l'indice del nodo da ricercare (minore di %d): ",size);
+        scanf("%d",&index);
+
+    }while(index>size);
+
+    printf("inserisci nuovo numero: ");
+    scanf("%d",&sostituto);
+  
+    node * temp=elementAt(index,head);
+    temp->elemento=sostituto;     //sostituisco il valore del nodo con l'indice ricercato.
 }
 
 /*
@@ -69,10 +111,12 @@ FUNZIONE printList:
     stampa tutti i nodi di una lista fino a quando non si arriva al nodo finale.*/
     
 void printList(node * head){
+    printf("\n\n");
     while (head!=NULL){             //finchè un nodo non punta a NULL (ovvero finchè non è l'ultimo nodo della lista).
         printf("%d: %d -> ",head->index,head->elemento);        //stampo indice e contenuto del nodo.
         head=head->next;        //passo al nodo successivo.
     }
+    printf("\n\n");
 }
 
 
@@ -86,6 +130,52 @@ node * elementAt(int indice, node * head){
     while (indice!=temp->index){        //rimane nel ciclo while finchè non trova un nodo con l'indice desiderato.
         temp=temp->next;
     }
-    printf("numero trovato alla posizione %d: %d\n",indice,temp->elemento);     //stampa il contenuto del nodo all'indice ricercato.
-    return temp;
+    return temp;        //ritorna il nodo che si trova nell'indice indicato.
+}
+
+/*
+FUNZIONE sizeList:
+    Ritorna alla funzione chiamante la grandezza della lista.
+*/
+int sizeList(node * head){
+    int i=0;
+    while (head!=NULL){
+        head=head->next;
+        i++;
+    }
+    return i;
+}
+
+/*
+FUNZIONE sortList:
+    data una lista come parametro, preleva i numeri contenuti all'interno dei i nodi della lista 
+    inserendoli all'interno di un vettore. Infine, li riordina con un semplice algoritmo di bubble sort.
+*/
+
+void sortList(node * head){
+    node * temp=head;               //creo nodo temporaneo
+    int size=sizeList(head);        //definisco la grandezza della lista grazie alla funzione sizelist
+    int vet[size];
+
+    for (int i=0;i<size;i++)        //for che controlla tutti i nodi della lista.
+    {
+        vet[i]=temp->elemento;          //inserisco l'elemento di un nodo all'interno del vettore.
+        temp=temp->next;                //passa al prossimo nodo
+    }
+
+    for (int i=0;i<size;i++){
+        for (int y=0;y<i;y++){
+            if (vet[y]>vet[i])              //ALGORITMO DI BUBBLE SORT.
+            {
+                int temp=vet[i];
+                vet[i]=vet[y];
+                vet[y]=temp;
+            }
+        }
+    }
+    printf("\n\nelementi della lista ordinati: ");     
+    for (int i=0;i<size;i++){           //stampo gli elementi del vettore, ora ordinati in modo crescente.
+        printf("%d; ",vet[i]);
+    }
+    printf("\n\n");
 }
