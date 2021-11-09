d19dd9eef150f0a0cfcc3383f22e52b556a59681
DENNIS.CORABI <CRBDNS04P28D416Q@info.windows2000>
2021-11-09 13:42:02 +0100

    finally the riot API usage

--- java/RiotAPI/src/main/java/RiotAPI.java
+++ java/RiotAPI/src/main/java/RiotAPI.java
@@ -1,25 +1,16 @@
 import com.google.gson.*;
-
-import java.util.UUID;
+import com.merakianalytics.orianna.Orianna;
+import com.merakianalytics.orianna.types.common.Region;
+import com.merakianalytics.orianna.types.core.summoner.Summoner;
 
 public class RiotAPI {
     public static void main(String[] args) {
+        Orianna.setRiotAPIKey("RGAPI-0f70d73c-dda1-44b2-b96c-b75dde785987");
+        Orianna.setDefaultRegion(Region.EUROPE_WEST);
+
         Gson gson = new Gson();
-        JsonObject user = new JsonObject();
-        JsonArray vet = new JsonArray();
-        JsonPrimitive field = new JsonPrimitive("sommo");
 
-        vet.add("sommo");
-        vet.add(3);
-        System.out.println(vet);
+        Summoner player = Summoner.named("denn7ls").withRegion(Region.EUROPE_WEST).get();
 
-        user.add("name",field);
-        user.addProperty("id", UUID.randomUUID().toString());
-
-        User dennis = gson.fromJson(user,User.class);
-        System.out.println(dennis.getName());
-        System.out.println(gson.toJson(new User("dennis")));
-        user =gson.toJsonTree(new User("u")).getAsJsonObject();
-        System.out.println(user);
     }
 }
