7427f912b0201cd19e1637a3a3cded599629b58a
Dennis <denniscorabi@outlook.com>
2021-11-25 12:48:43 +0100

    new JS project

--- javascript/demo/App.js
+++ javascript/demo/App.js
@@ -1,19 +1,37 @@
+window.onload = generateNum;
+$("#form").focus();
 
-function HTTPrequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method,url,true);
-    xhr.responseType = "json";
-    xhr.send();
+let findNum;
+let count=0;
 
-    xhr.onload = function () {
-        if (xhr.status==200){
-            console.log("done.");
-            console.log(xhr.response);
-        }
-        else
-            console.log("error " + xhr.status);
-    }
-    xhr.onprogress = function () {
-        console.log("still processing...");
-      }
+
+$("#newGame").click(generateNum);
+function generateNum() {
+    $("#responses").text("Numeri inseriti: ");
+    findNum=Math.floor((Math.random()*100)+1);
+    console.log(findNum);
 }
+
+
+$("#searchBT").click(find);
+function find() {
+    let num=$("#form").val();
+    if (parseInt(num)===findNum) {
+        console.log("hai vinto!");
+    }
+    else if(num>findNum)
+        console.log("numero scorretto, riprova (HINT: il numero da trovare è minore di "+num+")");
+    else
+        console.log("numero scorretto, riprova (HINT: il numero da trovare è maggiore di "+num+")");
+
+    count++;
+    $("#responses").text($("#responses").text()+" "+num+", ");
+    $("#counter").text("tentativi rimanenti: "+count);
+}
+
+
+//TODO AGGIUNGERE CONTROLLO CHE SI INSERISCA UN NUMERO E NON UNA STRINGA
+//TODO AGGIUNGERE LA POSSIBILITÀ ALL'UTENTE DI INSERIRE QUANTI TENTATIVI VUOLE E IL TETTO MASSIMO PER IL NUMERO DA GENERARE.
+
+
+
