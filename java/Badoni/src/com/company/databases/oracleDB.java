edfdfd04da56c734d302beddfe3102d098caa0d2
Dennis <denniscorabi@outlook.com>
2021-12-02 18:07:42 +0100

    DATABASE!

--- java/Badoni/src/com/company/databases/oracleDB.java
+++ java/Badoni/src/com/company/databases/oracleDB.java
@@ -1,12 +1,20 @@
 package com.company.databases;
-
 import java.sql.*;
 
 public class oracleDB{
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Connection conn= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
-
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/helloSQL","sommo","Idduiddu1!");
+        if (conn!=null){
+            System.out.println("connessione avvenuta con successo");
+        }
+        assert conn != null;
         Statement statement = conn.createStatement();
-        ResultSet rs=statement.executeQuery("CREATE TABLE dennis");
+        statement.execute("INSERT INTO Utenti values(\"rio\",\"cogno\",10)");
+        ResultSet rs=statement.executeQuery("SELECT * FROM Utenti");
+
+        while(rs.next()){
+            System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+        }
     }
 }
