c3309fe785edbeaaf99b3d42ad10573a18c0ab55
dennis <denniscorabi@outlook.com>
2022-02-23 18:52:49 +0100

    Programma CRC

---
+++ C#/CRC/CRC/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+void initialize()
+{
+    string BitSequence = "1011010";
+    string Divisore = FillWithZeros(BitSequence.Length, "1001");
+    Console.WriteLine($"Dividendo: {BitSequence}\t Divisore: {Divisore}");
+    Division(BitSequence, Divisore);
+}
+
+void Division(string dividendo, string divisore)
+{
+    char[] quoziente;
+    if (dividendo.Length != divisore.Length)
+        return;
+
+    for (int i=0; i<dividendo.Length; i++)
+    {
+        char x = (char) (dividendo[i] + divisore[i] );
+        if (x == 2) x = '0';
+
+        quoziente[i] = x;
+    }
+
+    Console.WriteLine(quoziente);
+   
+}
+
+string FillWithZeros(int lenght, string bitSeq)
+{
+    while (bitSeq.Length != lenght) bitSeq += "0";
+    return bitSeq;
+}
+
+initialize();
