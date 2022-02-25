390c211a5830fd974106ea1873d69d2d857657cf
dennis <denniscorabi@outlook.com>
2022-02-25 18:42:27 +0100

    New java project

---
+++ java/ContagiCovid/src/main/java/Day.java
@@ -0,0 +1,10 @@
+public class Day {
+    private String data;
+    private String stato;
+    private int ricoveratiConSintomi;
+    private int terapiaIntensiva;
+    private int totaleOspedalizzati;
+    private int isolamentoDomiciliare;
+    private int totalePositivi;
+    private int variazioneTotale;
+}
