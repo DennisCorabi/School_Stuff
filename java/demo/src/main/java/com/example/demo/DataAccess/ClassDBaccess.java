06b058040f2a87fda900067a637536f76453f6a5
Dennis <69604047+DennisCorabi@users.noreply.github.com>
2022-02-25 11:05:39 +0100

    Delete java/demo directory

--- java/demo/src/main/java/com/example/demo/DataAccess/ClassDBaccess.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.DataAccess;
-
-import com.example.demo.Model.Classi;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClassDBaccess extends CrudRepository<Classi, Long> {
-    Classi getClassiByAnnoAndSezioneAndArticolazione(String anno, String sezione, String articolazione);
-
-}
